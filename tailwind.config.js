3bb4acf516c503c7aec7c1a0749fef8a623bc0e8
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-02-28 09:56:09 +0700

    📦 configuring tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+            screens: {
+                'sm': '240px',
+                'md': '640px',
+                'lg': '800px',
+                'xl': '1024px',
+                '2xl': '1280px',
+                '3xl': '1680px',
+                '4xl': '1928px',
+            },
+        },
   },
   plugins: [],
 }
-
+;
