3f757a126da50c20ab4c210b4741828ab5b794f3
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-04-08 06:29:04 +0700

    🛠️ add 3d model for later usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from "@/shared/Navbar";
+import Mansion from "./components/models/Mansion";
 
 function App() {
     return (
         <div className='h-screen bg-gray-800'>
             <Navbar />
+            <Mansion />
         </div>
     );
 }
