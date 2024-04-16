f2125594ecfa083857ce41bf2e5174afbf3e5b8e
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-04-16 08:43:54 +0700

    📦 Added toast notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import {
-  RouterProvider,
+    RouterProvider,
 } from "react-router-dom";
+import { ToastContainer, toast, Bounce } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import router from "./router/router";
+import { useEffect } from "react";
 
 function App() {
+    useEffect(() => {
+        return () => {
+            toast.warn('Still in development!', {
+                position: "bottom-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                transition: Bounce,
+            });
+        };
+    }, []);
+
     return (
-        <div className='min-h-screen bg-black'>
+        <div className='min-h-screen bg-black grid grid-rows-[auto_1fr_auto]'>
             <RouterProvider router={router} />
+
+            <ToastContainer />
         </div>
     );
 }
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,12 @@
+import Layout from "@/shared/Layout"
+
 export default function NotFound() {
     return (
-        <main className="min-h-screen min-w-full flex flex-col justify-center items-center gap-8">
-            <h1 className="text-6xl font-bold font-mono tracking-widest text-red-600">404</h1>
-            <h2 className="text-4xl font-mono tracking-wide">Not Found</h2>
-        </main>
+        <Layout>
+            <main className="min-h-screen flex flex-col justify-center items-center gap-8">
+                <h1 className="text-6xl font-bold font-mono tracking-widest text-red-600">404</h1>
+                <h2 className="text-4xl font-mono text-white tracking-wide">Not Found</h2>
+            </main>
+        </Layout>
     );
 }
